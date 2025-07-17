4b94869497a3d5e2309b819a8b10aa18e31221ae
hikamabq <abqoryul@gmail.com>
2025-07-17 16:35:59 +0700

    update

--- components/post_1.php
+++ components/post_1.php
@@ -1,4 +1,4 @@
-<div class="border p-5 text-center <?= $model['style']['background'] ?> <?= $model['style']['color'] ?>">
+<div class="border p-5 rounded text-center <?= $model['style']['background'] ?> <?= $model['style']['color'] ?>">
     <div class="mb-3">
         <h3><?= $model['data']['title'] ?></h3>
         <span><?= $model['data']['subtitle'] ?></span>
--- modules/admin/views/layouts/admin.php
+++ modules/admin/views/layouts/admin.php
@@ -49,7 +49,9 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
                     <svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-layout-grid"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" /><path d="M14 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" /><path d="M4 14m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" /><path d="M14 14m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" /></svg>
                     <span class="ps-2">Dashboard</span>
                 </a>
+                <div class="mb-1 mt-3 px-2 very-small text-secondary text-uppercase">Pages</div>
                 <?= Menu::pages(); ?>
+                <div class="mb-1 mt-3 px-2 very-small text-secondary text-uppercase">Management</div>
                 <?= Menu::management(); ?>
             </div>
         </div>
