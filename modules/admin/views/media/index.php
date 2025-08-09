403b87f1d4c6440af3e3d86d8ca0fbbc63bbf180
hikamabq <abqoryul@gmail.com>
2025-08-09 11:07:04 +0700

    update media

--- modules/admin/views/media/index.php
+++ modules/admin/views/media/index.php
@@ -48,19 +48,21 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php Pjax::begin(); ?>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <div class="row mt-5 p-3">
-        <?php foreach($dataProvider->models as $data){ ?>
-        <div class="col-md-3 p-0">
-            <div class="card rounded-0 border-0">
-                <div class="card-body h-100 p-0">
-                    <img src="<?= Url::to('@web/uploads/'.$data['name'].'') ?>" class="img-fluid position-relative" alt="">
-                    <a href="<?= Url::to(['delete', 'id' => $data['id']]) ?>" class="position-absolute top-0 end-0 bg-white p-1" data-confirm="Are you sure you want to delete this item?" data-method="post">
-                        <svg  xmlns="http://www.w3.org/2000/svg"  width="16"  height="16"  viewBox="0 0 24 24"  fill="none"  stroke="red"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>
-                    </a>
+    <div class="p-3 mt-5">
+        <div class="row border">
+            <?php foreach($dataProvider->models as $data){ ?>
+            <div class="col-md-2 p-0">
+                <div class="card rounded-0 border-0">
+                    <div class="card-body h-100 p-0">
+                        <img src="<?= Url::to('@web/uploads/'.$data['name'].'') ?>" class="img-fluid position-relative" alt="">
+                        <a href="<?= Url::to(['delete', 'id' => $data['id']]) ?>" class="position-absolute top-0 end-0 bg-white p-1" data-confirm="Are you sure you want to delete this item?" data-method="post">
+                            <svg  xmlns="http://www.w3.org/2000/svg"  width="16"  height="16"  viewBox="0 0 24 24"  fill="none"  stroke="red"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>
+                        </a>
+                    </div>
                 </div>
             </div>
+            <?php } ?>
         </div>
-        <?php } ?>
     </div>
 
     <?php Pjax::end(); ?>
